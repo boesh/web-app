1c9eceaf85a0af8e0567f964aadd231486ca8284
Guts <rudenko.vitaliy1998@gmail.com>
2020-01-20 17:16:42 +0300

    api update

--- models/models.js
+++ models/models.js
@@ -113,73 +113,11 @@ Master.belongsToMany(City, {through: CitiesOfMasters, onDelete: "cascade"});
 City.belongsToMany(Master, {through: CitiesOfMasters, onDelete: "cascade"});
 
 
-FindUser = async(email) =>
-{
-  return await User.findOne({where: {email: email}})
-}
-
-UpdateUser = async (user, name, city) =>
-{
-  return await user.update({name: name, city: city})
-}
-
-
-FindMaster = async (email) => {
-  return await Master.findOne({where: {email: email}})
-}
-
-
-UpdateMaster = async (master, name, rating) => {
-  return await master.update({name: name, rating: rating})
-}
 
 
 
 
 
-FindCity = async (cityName) => {
-  return await City.findOne({where: cityName})
-}
-
-
-
-
-
-// FindUser("mail@gmail.com")
-// .then(user => {
-//   !user ? CreateUser("fgdh", "mail@gmail.com", "city")
-//   .then(user => console.log(user.id)) 
-// : console.log(user.id) })
-// User.create({name: "testuser", email: "testmail@gmail.com", city: "testcity"})
-// User.create({name: "testuser1", email: "testmail1@gmail.com", city: "testcity1"})
-// User.create({name: "testuser2", email: "testmail2@gmail.com", city: "testcity2"})
-// AddUser("Oleg", "vi123@gmail.com", "Dnipro2")
-// AddMaster("master8@gmail.com", "master", 1).then(res => {
-//   console.log(res + " in promise")
-// })
-// AddUser("Oleg2", "victor439977852@gmail.com", "Dnipro2").then(userId => {
-//   console.log(userId)
-// })
-// AddUser("Oleg", "victor125@gmail.com", "Dnipro2").then(userId => {
-//   console.log(userId)
-// })
-
-  
-
-
-// AddOrder(Date.now(), 23, 3, 1, 5)
-// Master.findOne({where: {id: 5}})
-//   .then(master=> {
-//     if(!master) return;
-//     User.findOne({where: {id: 5}})
-//     .then(user=>{
-//       if(!user) return;
-//       master.addUser(user, {through: {date: Date.now(), hour: 23, watchsize: 3}})
-//       .then(order=>{
-//         console.log(order)
-//       })
-//     })
-//   })
 
 
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,13 +13,13 @@ module.exports = function(app) {
 
   
 
-  app.post("/api/adduser", function(req, res) {
-    models.User.findOne({where: {email: req.body.email}})
+  app.post("/api/add/user", function(req, res) {
+    models.User.findOne({where: {email: req.query.email}})
     .then(user => {
       if(!user)
       {
-        models.User.create({name: req.body.name,
-           email: req.body.email, city: req.body.city})
+        models.User.create({name: req.query.name,
+           email: req.query.email, city: req.query.city})
         .then(createdUser => {
           res.json(createdUser.id)
         })
@@ -30,29 +30,40 @@ module.exports = function(app) {
     })
   })
 
- 
+  app.post("/api/admin/edit/user", function(req, res) {
+    models.Master.update({name: req.query.name, city: req.query.city, email: req.query.email},
+      {
+        where: {
+          id: req.query.id
+        }
+      })
+    .then(result => 
+      {
+        res.json(result)
+      })
+  });
 
-  app.post("/api/admin/deleteuser", function(req, res) {
-    models.User.destroy({where: {id: req.body.userId}})
+  app.post("/api/admin/delete/user", function(req, res) {
+    models.User.destroy({where: {id: req.query.userId}})
     .then(destroyedRows => {
       res.json(destroyedRows)
     })
   })
 
-  app.post("/api/addorder", function(req, res) {
-    models.Master.findOne({where: {id: req.body.masterId}})
+  app.post("/api/add/order", function(req, res) {
+    models.Master.findOne({where: {id: req.query.masterId}})
     .then(master => {
       if(!master) res.json(404);
-      models.User.findOne({where: {id: req.body.userId}})
+      models.User.findOne({where: {id: req.query.userId}})
       .then(user => {
         if(!user) res.json(404);
-        models.Order.findOne({where: {masterId: req.body.masterId,
-           date: req.body.date}})
+        models.Order.findOne({where: {masterId: req.query.masterId,
+           date: req.query.date}})
         .then(order => {
           if(!order) {
-          models.Order.create({date: req.body.date, 
-            watchsize: req.body.watchsize, userId: req.body.userId,
-             masterId: req.body.masterId})
+          models.Order.create({date: req.query.date, 
+            watchsize: req.query.watchsize, userId: req.query.userId,
+             masterId: req.query.masterId})
           .then(order => res.json(order))
           }
           else {
@@ -63,8 +74,29 @@ module.exports = function(app) {
     })
   });
 
-  app.post("/api/confirmorder", function(req, res) {
-    models.Order.findOne({where: {id: req.body.orderId, confirmed: false}})
+  app.post("/api/admin/delete/order", function(req, res) {
+    models.Order.destroy({where: {id: req.query.userId}})
+    .then(destroyedRows => {
+      res.json(destroyedRows)
+    })
+  })
+
+  app.post("/api/admin/edit/order", function(req, res) {
+    models.Master.update({date: req.query.date, watchsize: req.query.watchsize, 
+      confirmed: req.query.confirmed, userId: req.query.userId, masterId: req.query.masterId},
+      {
+        where: {
+          id: req.query.id
+        }
+      })
+    .then(result => 
+      {
+        res.json(result)
+      })
+  });
+
+  app.post("/api/confirm_order", function(req, res) {
+    models.Order.findOne({where: {id: req.query.id, confirmed: false}})
     .then(order => {
       if(!order) res.json(404)
       order.update({confirmed: true})
@@ -72,28 +104,41 @@ module.exports = function(app) {
     })
   });
 
-  app.post("/api/admin/addcity", function(req, res) {
-    models.City.findOrCreate({ where: {name: req.body.name}})
+  app.post("/api/admin/add/city", function(req, res) {
+    models.City.findOrCreate({ where: {name: req.params.name}})
     .then((model, modelHasBeenCreated) => {
       res.json(model)
     })
   });
 
-  app.post("/api/admin/deletecity", function(req, res) {
-    models.City.destroy({where: {name: req.body.name}})
+  app.post("/api/admin/edit/city", function(req, res) {
+    models.Master.update({name: req.query.name},
+      {
+        where: {
+          id: req.query.id
+        }
+      })
+    .then(result => 
+      {
+        res.json(result)
+      })
+  });
+
+  app.post("/api/admin/delete/city", function(req, res) {
+    models.City.destroy({where: {id: req.query.id}})
     .then(destroyedRows => {
       res.json(destroyedRows)
     })
   });
 
-  app.post("/api/admin/addmaster", function(req, res) {
-    models.Master.findOne({where: {email: req.body.email}})
+  app.post("/api/admin/add/master", function(req, res) {
+    models.Master.findOne({where: {email: req.query.email}})
     .then(master => {
     if(!master) {
-      models.Master.create({name: req.body.name,
-          email: req.body.email, rating: req.body.rating})
+      models.Master.create({name: req.query.name,
+          email: req.query.email, rating: req.query.rating})
       .then(mas => {
-        for (city of req.body.cities)
+        for (city of req.query.cities)
         {
           mas.addCity(city)
         }
@@ -105,20 +150,68 @@ module.exports = function(app) {
     }})
   })
 
+  app.post("/api/admin/edit/master", function(req, res) {
+    models.Master.update({name: req.query.name, rating: req.query.rating, email: req.query.email},
+      {
+        where: {
+          id: req.query.id
+        }
+      })
+    .then(result => 
+      {
+        res.json(result)
+      })
+  });
 
-  app.post("/api/admin/deletemaster", function(req, res) {
-    master.Master.destroy({where: {id: req.body.masterId}})
+
+  app.post("/api/admin/delete/master", function(req, res) {
+    models.Master.destroy({where: {id: req.query.id}})
     .then(destroyedRows => {
       res.json(destroyedRows)
     })
   });
 
+  app.get("/api/masters", function(req, res) {
+    models.Master.findAll({limit: req.query.limit, offset: req.query.offset}).then(masters => res.json(masters))
+  });
+
+  app.get("/api/admin/users", function(req, res) {
+    models.User.findAll({limit: req.query.limit, offset: req.query.offset}).then(users => res.json(users))
+  });
+
+  app.get("/api/admin/orders", function(req, res) {
+    models.Order.findAll({limit: req.query.limit, offset: req.query.offset}).then(orders => res.json(orders))
+  });
+
+  app.get("/api/cities", function(req, res) {
+    models.City.findAll({limit: req.query.limit, offset: req.query.offset}).then(cities => res.json(cities))
+  });
+
+  app.get("/api/master", function(req, res) {
+    models.Master.findOne({where: {id: req.query.id}}).then(master => res.json(master))
+  });
+
+  app.get("/api/admin/user", function(req, res) {
+    models.User.findOne({where: {id: req.query.id}}).then(user => res.json(user))
+  });
+
+  app.get("/api/admin/order", function(req, res) {
+    models.Order.findOne({where: {id: req.query.id}}).then(order => res.json(order))
+  });
+
+  app.get("/api/city", function(req, res) {
+    models.City.findOne({where: {id: req.query.id}}).then(city => res.json(city))
+    
+  });
+
+  
+
+
 
   app.post("/api/signup", function(req, res) {
-    console.log(req.body);
     adminsdb.create({
-      email: req.body.email,
-      password: req.body.password
+      email: req.query.email,
+      password: req.query.password
     }).then(function() {
       res.redirect(307, "/api/login");
     }).catch(function(err) {
